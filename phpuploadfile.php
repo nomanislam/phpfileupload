616ff33ff501f1ead18efaf9d9202ba5fc5700ad
Noman Islam <36672487+nomanislam@users.noreply.github.com>
2019-04-28 01:10:43 +0600

    Update phpuploadfile.php

--- phpuploadfile.php
+++ phpuploadfile.php
@@ -1,39 +1,28 @@
 
 
 
+$file_name = $_FILES["photo"]["name"];
+$file_type = $_FILES["photo"]["type"];
+$file_tmp = $_FILES["photo"]["tmp_name"];
+$file_error = $_FILES["photo"]["error"];
+$file_size = $_FILES["photo"]["size"];
 
-<form action="registraData.php" method="post" enctype="multipart/form-data">
 
-  
-  <input type ="file" name="photo">
-  
-  <input type="submit" value="Submit">
-  
-  
-  
-  </form> 
-  
-  
-  
-  
-  
- <?php
- 
- $type = explode(".", $_FILES["photo"]["name"]);
- $type = $type [count($type)-1];
- $url = "./imagesup/".uniqid(rand()).".".$type;
- 
+
+$ary = explode(".", $file_name);
+$extention = $ary[count($ary)-1];
+
+$url = uniqid(rand()).".".$extention;
+
+
+
+if(in_array($extention,array("jpg","JPG","png","PNG"))){
+	if(is_uploaded_file($_FILES["photo"]["tmp_name"])){
+		if(move_uploaded_file($_FILES["photo"]["tmp_name"],"imagesup/".$url)){
+			echo "Thsi file is Uploaded";
+		}
+	}
+}else{
+	echo "Data Type Dose not Match";
+}
 	
- if(in_array($type,array("JPG","jpg","PNG","png"))){
-	 if(is_uploaded_file($_FILES["photo"]["tmp_name"])){
-		 if(move_uploaded_file($_FILES["photo"]["tmp_name"], $url))
-			 echo "successful Upload";
-	 }
- }else{
-	 echo "Something is Worng";
- }
-
-
- 
- ?>
-  
